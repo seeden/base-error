1e3d1c4c308184bce7d692a6a4409eca1c4dde57
Zlatko Fedor <zfedor@gmail.com>
2014-04-26 15:27:06 +0200

    added package

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 'use strict';
 
+var util = require("util");
+
 function BaseError (message) {
-	//Error.call(this);
+	Error.call(this);
   	//Error.captureStackTrace(this, arguments.callee);
 
-    this.message = message;
-    this.stack = (new Error()).stack;
+    //this.message = message;
+    //this.stack = (new Error()).stack;
 }
 
-BaseError.prototype = new Error; 
+util.inherits(BaseError, Error);
+
+//BaseError.prototype = new Error; 
 
 
 module.exports = BaseError;
